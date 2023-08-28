9b4ee5f740b2e3aa1fa16cf3ac83bb1dd80cd8e0
jergauth <jeremiegthr@gmail.com>
2023-08-28 23:44:48 +0200

    fix: doc for keysOf

--- src/object/keys-of.ts
+++ src/object/keys-of.ts
@@ -2,8 +2,7 @@
  * Returns the names of the enumerable string properties and methods of an object.
  * (Note: This is just a typed wrapper around the native Object.keys method)
  * @param source Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
- * @template K Type of the keys that composed the `source`.
- * @template V Type of the values that composed the `source`.
+ * @template Key Type of the keys that composed the `source`.
  * @returns An array of keys.
  */
 export const keysOf = <Key extends keyof any>(
