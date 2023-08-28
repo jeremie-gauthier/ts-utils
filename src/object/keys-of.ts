a59312e8cfb549247363f1887c05d3aa20dcf147
jergauth <jeremiegthr@gmail.com>
2023-08-28 23:34:08 +0200

    refactor: typings of keysOf

--- src/object/keys-of.ts
+++ src/object/keys-of.ts
@@ -1,11 +1,11 @@
 /**
  * Returns the names of the enumerable string properties and methods of an object.
  * (Note: This is just a typed wrapper around the native Object.keys method)
- * @param object Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
- * @template K Type of the keys that composed the `object`.
- * @template V Type of the values that composed the `object`.
+ * @param source Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+ * @template K Type of the keys that composed the `source`.
+ * @template V Type of the values that composed the `source`.
  * @returns An array of keys.
  */
-export const keysOf = <K extends keyof any, V>(
-  object: Readonly<Record<K, V>>,
-): (keyof typeof object)[] => Object.keys(object) as (keyof typeof object)[];
+export const keysOf = <Key extends keyof any>(
+  source: Readonly<Record<Key, unknown>>,
+): Key[] => Object.keys(source) as Key[];
