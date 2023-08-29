5f5899df55380824f39c89ed41512f5c95d6cb32
jergauth <jeremiegthr@gmail.com>
2023-08-29 23:03:18 +0200

    refactor: isInRange to be tolerant w/ args order

--- src/number/is-in-range.spec.ts
+++ src/number/is-in-range.spec.ts
@@ -1,21 +1,43 @@
 import { isInRange } from './is-in-range';
 
 describe('number: isInRange', () => {
-  it('should return true if min <= number <= max', () => {
-    const lowerBound = 0;
-    const upperBound = 3;
+  describe('when min <= number <= max', () => {
+    it('should return true', () => {
+      const lowerBound = 0;
+      const upperBound = 3;
 
-    expect(isInRange(0, lowerBound, upperBound)).toBe(true);
-    expect(isInRange(1, lowerBound, upperBound)).toBe(true);
-    expect(isInRange(2, lowerBound, upperBound)).toBe(true);
-    expect(isInRange(3, lowerBound, upperBound)).toBe(true);
+      expect(isInRange(0, lowerBound, upperBound)).toBe(true);
+      expect(isInRange(1, lowerBound, upperBound)).toBe(true);
+      expect(isInRange(2, lowerBound, upperBound)).toBe(true);
+      expect(isInRange(3, lowerBound, upperBound)).toBe(true);
+    });
+
+    it('should still works when args are mixed up', () => {
+      const lowerBound = 0;
+      const upperBound = 3;
+
+      expect(isInRange(0, upperBound, lowerBound)).toBe(true);
+      expect(isInRange(1, upperBound, lowerBound)).toBe(true);
+      expect(isInRange(2, upperBound, lowerBound)).toBe(true);
+      expect(isInRange(3, upperBound, lowerBound)).toBe(true);
+    });
   });
 
-  it('should return false if number not in range [min, max]', () => {
-    const lowerBound = 0;
-    const upperBound = 3;
+  describe('when number not in range [min, max]', () => {
+    it('should return false if number not in range [min, max]', () => {
+      const lowerBound = 0;
+      const upperBound = 3;
 
-    expect(isInRange(-1, lowerBound, upperBound)).toBe(false);
-    expect(isInRange(4, lowerBound, upperBound)).toBe(false);
+      expect(isInRange(-1, lowerBound, upperBound)).toBe(false);
+      expect(isInRange(4, lowerBound, upperBound)).toBe(false);
+    });
+
+    it('should still works when args are mixed up', () => {
+      const lowerBound = 0;
+      const upperBound = 3;
+
+      expect(isInRange(-1, upperBound, lowerBound)).toBe(false);
+      expect(isInRange(4, upperBound, lowerBound)).toBe(false);
+    });
   });
 });
--- src/number/is-in-range.ts
+++ src/number/is-in-range.ts
@@ -1,12 +1,18 @@
 /**
- * Checks if number is between min and max (inclusive range).
+ * Checks if number is between start and stop (inclusive range).
  * @param number The number to check.
- * @param min The start of the range.
- * @param max The end of the range.
+ * @param start The start of the range.
+ * @param stop The end of the range.
  * @returns Returns true if number is in the range, else false.
  * @example
  * isInRange(42, 40, 50);
  * // => true
  */
-export const isInRange = (number: number, min: number, max: number): boolean =>
-  number >= min && number <= max;
+export const isInRange = (
+  number: number,
+  start: number,
+  stop: number,
+): boolean => {
+  const [min, max] = start <= stop ? [start, stop] : [stop, start];
+  return min <= number && number <= max;
+};
