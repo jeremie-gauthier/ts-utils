b449624be50c5ba78345a5438a555b6c71ad59a5
jergauth <jeremiegthr@gmail.com>
2023-08-30 00:30:30 +0200

    docs: add example on sum

--- src/math/sum.ts
+++ src/math/sum.ts
@@ -4,6 +4,9 @@ const INITIAL_VALUE = 0;
  * Compute the sum of the given numbers.
  * @param numbers The numbers to sum.
  * @returns The sum of the numbers.
+ * @example
+ * sum(1, 2, 3, 4, 5);
+ * // => 15
  */
 export const sum = (...numbers: number[]): number =>
   numbers.reduce(
