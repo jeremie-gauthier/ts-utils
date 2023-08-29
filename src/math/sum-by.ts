a67236864565ae477a37618c571b78916a89fafb
jergauth <jeremiegthr@gmail.com>
2023-08-29 23:09:31 +0200

    refactor: var naming

--- src/math/sum-by.ts
+++ src/math/sum-by.ts
@@ -9,6 +9,7 @@ export type Iteratee<Item> = (item: Item) => number;
 
 /**
  * Compute the sum of the given numbers by applying the given iteratee on every item.
+ * The result of the iteratee function is then added to the sum.
  * @param iteratee The iteratee invoked per item.
  * @param numbers The numbers to sum.
  * @template Item Type of the `numbers` elements.
@@ -18,7 +19,4 @@ export const sumBy = <Item>(
   iteratee: Iteratee<Item>,
   ...numbers: Item[]
 ): number =>
-  numbers.reduce(
-    (previousValue, currentValue) => previousValue + iteratee(currentValue),
-    INITIAL_VALUE,
-  );
+  numbers.reduce((total, item) => total + iteratee(item), INITIAL_VALUE);
