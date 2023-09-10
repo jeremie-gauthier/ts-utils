7c071a6bf8fd20270530eb289670170b62d2d3b6
jergauth <jeremiegthr@gmail.com>
2023-09-10 22:25:51 +0200

    refactor: enhance capitalize return type

--- src/string/capitalize.ts
+++ src/string/capitalize.ts
@@ -6,5 +6,7 @@
  * capitalize("typescript");
  * // => "Typescript"
  */
-export const capitalize = (string: string): string =>
-  string.charAt(0).toUpperCase() + string.slice(1);
+export const capitalize = <Input extends string>(
+  string: Input,
+): Capitalize<Input> =>
+  (string.charAt(0).toUpperCase() + string.slice(1)) as Capitalize<Input>;
